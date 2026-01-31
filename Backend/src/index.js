a764f30eebc4b684683bdd90b8f3144fa555493b
Rushendra J <rushendra1729@gmail.com>
2026-01-31 13:00:09 +0530

    Fix SPA fallback using Express middleware

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -23,9 +23,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname,"../../Frontend/dist")));
-    app.get("/*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../../Frontend","dist","index.html"));
-    })
+    app.use((req, res) => {
+        res.sendFile(
+            path.join(__dirname, "../../Frontend", "dist", "index.html")
+        );
+    });
 }
 
 server.listen(PORT,()=> {
